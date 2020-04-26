752e428c21b7a0190798ac2088a879279d0a0d70
Anup-Shrestha <anupshresth9@gmail.com>
2020-04-26 19:41:36 +0545

    validation completed

---
+++ static/js/form_script.js
@@ -0,0 +1,40 @@
+const registerform = document.forms['register-form'];
+const email = registerform['email'];
+const password = registerform['password'];
+const rePassword = registerform['re-password'];
+const mobileNumber = registerform['mobileNumber'];
+const errorDiv = document.getElementById('error');
+
+function message(msg, e) {
+  e.preventDefault();
+  errorDiv.innerText = msg;
+  errorDiv.style.color = 'red';
+}
+
+registerform.addEventListener('submit', (e) => {
+  if (!validateEmail(email.value)) {
+    message('Enter Valid Email', e);
+    return false;
+  }
+  if (password.value.length < 6) {
+    message('Password must be atleast of 8 characters', e);
+    return false;
+  }
+  if (password.value === rePassword.value) {
+    message("Password don't Match", e);
+    return false;
+  }
+  if (!validateNumber(mobileNumber.value)) {
+    message('Enter valid Mobile Number');
+    return false;
+  }
+});
+
+function validateEmail(email) {
+  const re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+  return re.test(email);
+}
+function validateNumber(phoneNumber) {
+  const re = /^\d{10}$/;
+  return re.test(phoneNumber);
+}
