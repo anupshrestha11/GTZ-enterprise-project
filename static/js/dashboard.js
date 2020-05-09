71ad274896b1ad50361ae53750511b09b90dd92d
Anup-Shrestha <anupshresth9@gmail.com>
2020-05-09 15:05:01 +0545

    product page and add product page added

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -21,3 +21,45 @@ document.querySelector(".btn-toggle-menu").addEventListener(
   },
   true
 );
+
+//! ckeditor
+
+ClassicEditor.create(document.querySelector("#editor"), {
+  toolbar: {
+    items: [
+      "bold",
+      "italic",
+      "link",
+      "bulletedList",
+      "numberedList",
+      "|",
+      "indent",
+      "outdent",
+      "|",
+      "blockQuote",
+      "insertTable",
+      "undo",
+      "redo",
+      "alignment",
+    ],
+  },
+  language: "en",
+  image: {
+    toolbar: ["imageTextAlternative", "imageStyle:full", "imageStyle:side"],
+  },
+  table: {
+    contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"],
+  },
+  licenseKey: "",
+})
+  .then((editor) => {
+    window.editor = editor;
+  })
+  .catch((error) => {
+    console.error("Oops, something gone wrong!");
+    console.error(
+      "Please, report the following error in the https://github.com/ckeditor/ckeditor5 with the build id and the error stack trace:"
+    );
+    console.warn("Build id: 2ktte3oxf21f-4j0omid7h9rd");
+    console.error(error);
+  });
